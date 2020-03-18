011d1eae0c0e824fb619ed05dc0021a683950437
Sirbutter <mik.musinov@gmail.com>
2020-03-18 10:38:08 +0300

    Hello

--- complex.rb
+++ complex.rb
@@ -1,8 +1,17 @@
-class complex
+class 'Complex'
+    
     def initialize(real, imaginary)
         @real = Float(real)
         @imaginary = Float(imaginary)
     end
+
     def to_s
-        
+        String(real + i * imaginary)
+    end
+    def real
+        @real
+    end
+    def imaginary
+        @imaginary
+    end
 end
--- main.rb
+++ main.rb
@@ -1,1 +1,4 @@
-require_relative complex.rb
+require_relative 'complex'
+c1 = Сomplex.new(15, 14)
+c2 = Сomplex.new(44, 33)
+c1.to_s
